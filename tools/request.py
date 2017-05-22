5840b9a74e9f8a38e902d379b243f085ce8eacdc
otofune <otofune@gmail.com>
2017-05-22 23:09:45 +0900

    tools.request: initialize request class

---
+++ tools/request.py
@@ -0,0 +1,26 @@
+# -*- cording: utf-8 -*-
+
+import urllib.parse
+import urllib.request
+import json
+
+class Request:
+    def __init__ (self, config):
+        self.base_url = urllib.parse.urljoin(config.get('mastodon_url', 'meta'), '/api/v1/')
+        self.token = config.get('access_token')
+        self.headers = {
+            'content-type': 'application/json',
+            'user-agent': 'tootStdlibPy3 +https://github.com/BindEmotions/tootStdlibPy3'
+        }
+
+    def post (self, endpoint, data):
+        request_url = urllib.parse.urljoin(self.base_url, endpoint)
+        request_payload = json.dumps(data)
+        request = urllib.request.Request(
+            request_url,
+            request_payload.encode('utf-8'),
+            self.headers
+        )
+        with request as res:
+            data = json.loads(res.read())
+        return data
